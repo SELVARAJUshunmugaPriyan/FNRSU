8374a3209eab1a0c5513bcb5c517c0fcb9e2dd30
SELVARAJUshunmugaPriyan <shanmu.thukaram@gmail.com>
2018-07-23 19:13:39 +0200

    server.cpp: code clean-up

--- ORG/server.cpp
+++ ORG/server.cpp
@@ -1,28 +1,35 @@
+/* The port number is passed as an argument */
+#include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <stdio.h>
-#include <netdb.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+
+#include <netdb.h>
 #include <iostream>
 #include <fstream>
 #include <strings.h>
-#include <stdlib.h>
 #include <string>
 
 using namespace std;
 
 void taskContainer();
 
-static int connFd;
+void error(const char *msg)
+{
+  perror(msg);
+  exit(1);
+}
 
 int main(int argc, char* argv[])
 {
-  int pID, portNo, listenFd;
-  socklen_t len; //store size of the address
-  bool loop = false;
-  struct sockaddr_in svrAdd, clntAdd;
+  int sockfd, newsockfd, portno; // pID, portNo, sockfd;
+  socklen_t clilen; //store size of the address
+  char buffer[256];
+  struct sockaddr_in svr_addr, cli_addr;
+  int n;
 
   pthread_t threadA[3];
 
@@ -32,56 +39,71 @@ int main(int argc, char* argv[])
     return 0;
   }
 
-  portNo = atoi(argv[1]);
-
-  if((portNo > 65535) || (portNo < 2000))
-  {
-    cerr << "Please enter a port number between 2000 - 65535" << endl;
-    return 0;
-  }
-
-  //create socklen
-  listenFd = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
-
-  if(listenFd < 0)
+  // create a socket
+  // socket(int domain, int type, int protocol)
+  sockfd = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+  if(sockfd < 0)
   {
     cerr << "Cannot open socket" << endl;
     return 0;
   }
 
-  bzero((char*) &svrAdd, sizeof(svrAdd));
+  bzero((char*) &svr_addr, sizeof(svr_addr));
+  /* setup the host_addr structure for use in bind call */
+  // server byte order
+  svr_addr.sin_family = AF_INET;
 
-  svrAdd.sin_family = AF_INET;
-  svrAdd.sin_addr.s_addr = INADDR_ANY;
-  svrAdd.sin_port = htons(portNo);
+  // automatically be filled with current host's IP address
+  svr_addr.sin_addr.s_addr = INADDR_ANY;
 
-  //bind socket
-  if(bind(listenFd, (struct sockaddr *)&svrAdd, sizeof(svrAdd)) < 0)
+  // convert short integer value for port must be converted into network byte order
+  portno = atoi(argv[1]);
+  if((portno > 65535) || (portno < 2000))
+  {
+    cerr << "Please enter a port number between 2000 - 65535" << endl;
+    return 0;
+  }
+  svr_addr.sin_port = htons(portno);
+
+  // bind(int fd, struct sockaddr *local_addr, socklen_t addr_length)
+  // bind() passes file descriptor, the address structure,
+  // and the length of the address structure
+  // This bind() call will bind the socket to the current IP address on port, portno
+  if(bind(sockfd, (struct sockaddr *) &svr_addr, sizeof(svr_addr)) < 0)
   {
     cerr << "Cannot bind" << endl;
     return 0;
   }
 
-  listen(listenFd, 5);
+  // This length() call tells the socket to listen to the incoming connections.
+  // The listen() function places all incoming connection into a backlog queue
+  // until accept() call accepts the connection.
+  // Here, we set the maximum size for the backlog queue to 5.
+  listen(sockfd, 5);
 
   int noContnr = 0;
 
   while (noContnr < 3)
   {
-    len = sizeof(clntAdd);
+    clilen = sizeof(cli_addr);
     cout << "listening" << endl;
 
-    //this is where client connects. svr will hang in this mode until client connects
-    connFd = accept(listenFd, (struct sockaddr *)&clntAdd, &len);
+    // This accept() function will write the connecting client's address info
+    // into the address structure and the size of that structure is clilen.
+    // The accept() returns a new socket file descriptor for the accepted connection.
+    // So, the original socket file descriptor can continue to be used
+    // for accepting new connections while the new sokcet file descriptor is used for
+    // communication with the connected client.
+    newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
 
-    if (connFd < 0)
+    if (newsockfd < 0)
     {
       cerr << "Cannot accept connection" << endl;
       return 0;
     }
     else
     {
-      cout << "Connection successful" << endl;
+      cout << "server: got connection from " << inet_ntoa(cli_addr.sin_addr) <<  " port " <<  ntohs(cli_addr.sin_port) << endl;
     }
 
     taskContainer();
@@ -90,7 +112,7 @@ int main(int argc, char* argv[])
   }
 
   cout << "\nClosing thread and conn" << endl;
-  close(connFd);
+  close(newsockfd);
 }
 
 void taskContainer ()
