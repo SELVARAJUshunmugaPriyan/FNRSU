240ec68484ce51a81b63487e9a748df7b0cce9a2
SELVARAJUshunmugaPriyan <shanmu.thukaram@gmail.com>
2018-07-25 20:14:58 +0200

    ping tested container deployments

--- ORG/client.cpp
+++ ORG/client.cpp
@@ -22,11 +22,11 @@ using namespace std;
 
 int main (int argc, char* argv[])
 {
-  int sockfd, portno;
+  int sock_f_dsc, port_no;
   struct sockaddr_in serv_addr;
   struct hostent *server;
+  char buffer[255];
 
-  char buffer[256];
   if(argc < 3)
   {
     cerr << "Syntax : ./client <host name> <port>" << endl;
@@ -35,8 +35,8 @@ int main (int argc, char* argv[])
 
   // create a socket
   // socket(int domain, int type, int protocol)
-  sockfd = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
-  if(sockfd < 0)
+  sock_f_dsc = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+  if(sock_f_dsc < 0)
   {
     cerr << "Cannot open socket" << endl;
     return 0;
@@ -57,22 +57,29 @@ int main (int argc, char* argv[])
 
   bcopy((char *) server -> h_addr, (char *) &serv_addr.sin_addr.s_addr, server -> h_length);
   // convert short integer value for port must be converted into network byte order
-  portno = atoi(argv[2]);
-  if((portno > 65535) || (portno < 2000))
+  port_no = atoi(argv[2]);
+  if((port_no > 65535) || (port_no < 2000))
   {
     cerr << "Please enter port number between 2000 - 65535" << endl;
     return 0;
   }
-  serv_addr.sin_port = htons(portno);
+  serv_addr.sin_port = htons(port_no);
 
   // Connect the socket to the address of the server using the connect() system call.
-  int checker = connect(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr));
+  int checker = connect(sock_f_dsc, (struct sockaddr *) &serv_addr, sizeof(serv_addr));
   if (checker < 0)
   {
     cerr << "Cannot connect!" << endl;
     return 0;
   }
 
+  bzero(buffer, 256);
+
   // send ID to server
-  send(sockfd, "9751914896", 10, 0);
+  send(sock_f_dsc, "9751914896", 10, 0);
+  read(sock_f_dsc, buffer, 255);
+
+  cout << buffer << endl;
+  system((string("ping ") + buffer).c_str());
+
 }
--- ORG/server.cpp
+++ ORG/server.cpp
@@ -22,19 +22,21 @@
 
 using namespace std;
 
-void tsk_cntr();
+string tsk_cntr();
+string exec(const char* cmd);
 
 // system command for deploying container. xxxxxxxxxxxx -> container name
-string CMD("docker run --name xxxxxxxxxxxx testRun --network fn-cntr-int-net -dit alpine sh > /dev/null && docker inspect --format='{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' xxxxxxxxxxxx");
+string CMD("docker run --name xxxxxxxxxxxx --network fn-cntr-int-net -dit alpine sh > /dev/null && docker inspect --format='{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' xxxxxxxxxxxx");
 
 int main(int argc, char* argv[])
 {
-  int sock_fl_desc, nov_sock_fl_desc, port_no;
+  int sock_f_dsc, nv_sock_f_dsc, port_no;
   socklen_t clnt_len; //store size of the address
   char buffer[256];
   int no_cntr = 0;
   struct sockaddr_in svr_addr, cli_addr;
   int no_rcvd_bts;
+  string cntr_ip = "XXX.XXX.XXX.XXX";
 
   // acquire time
   time_t t;
@@ -49,8 +51,8 @@ int main(int argc, char* argv[])
 
   // create a socket
   // socket(int domain, int type, int protocol)
-  sock_fl_desc = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
-  if(sock_fl_desc < 0)
+  sock_f_dsc = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+  if(sock_f_dsc < 0)
   {
     cerr << "Cannot open socket" << endl;
     return 0;
@@ -64,7 +66,8 @@ int main(int argc, char* argv[])
   // automatically be filled with current host's IP address
   svr_addr.sin_addr.s_addr = INADDR_ANY;
 
-  // convert short integer value for port must be converted into network byte order
+  // convert short integer value for port must be converted into network byte
+  // order
   port_no = atoi(argv[1]);
   if((port_no > 65535) || (port_no < 2000))
   {
@@ -76,8 +79,9 @@ int main(int argc, char* argv[])
   // bind(int fd, struct sockaddr *local_addr, socklen_t addr_length)
   // bind() passes file descriptor, the address structure,
   // and the length of the address structure
-  // This bind() call will bind the socket to the current IP address on port, port_no
-  if(bind(sock_fl_desc, (struct sockaddr *) &svr_addr, sizeof(svr_addr)) < 0)
+  // This bind() call will bind the socket to the current IP address on port,
+  // port_no
+  if(bind(sock_f_dsc, (struct sockaddr *) &svr_addr, sizeof(svr_addr)) < 0)
   {
     cerr << "Cannot bind" << endl;
     return 0;
@@ -87,7 +91,7 @@ int main(int argc, char* argv[])
   // The listen() function places all incoming connection into a backlog queue
   // until accept() call accepts the connection.
   // Here, we set the maximum size for the backlog queue to 5.
-  listen(sock_fl_desc, 5);
+  listen(sock_f_dsc, 5);
 
   while (no_cntr < 1)
   {
@@ -96,31 +100,34 @@ int main(int argc, char* argv[])
 
     // This accept() function will write the connecting client's address info
     // into the address structure and the size of that structure is clnt_len.
-    // The accept() returns a new socket file descriptor for the accepted connection.
-    // So, the original socket file descriptor can continue to be used
-    // for accepting new connections while the new sokcet file descriptor is used for
-    // communication with the connected client.
-    nov_sock_fl_desc = accept(sock_fl_desc, (struct sockaddr *) &cli_addr, &clnt_len);
+    // The accept() returns a new socket file descriptor for the accepted
+    // connection. So, the original socket file descriptor can continue to be
+    // used for accepting new connections while the new sokcet file descriptor
+    // is used for communication with the connected client.
+    nv_sock_f_dsc = accept(sock_f_dsc, (struct sockaddr *) &cli_addr,
+      &clnt_len);
 
-    if (nov_sock_fl_desc < 0)
+    if (nv_sock_f_dsc < 0)
     {
       cerr << "Cannot accept connection" << endl;
       return 0;
     }
     else
     {
-      cout << "server: got connection from " << inet_ntoa(cli_addr.sin_addr) <<  " port " <<  ntohs(cli_addr.sin_port) << endl;
+      cout << "server: got connection from " << inet_ntoa(cli_addr.sin_addr) <<
+       " port " <<  ntohs(cli_addr.sin_port) << endl;
     }
 
     bzero(buffer, 256);
 
-    no_rcvd_bts = read(nov_sock_fl_desc, buffer, 255);
+    no_rcvd_bts = read(nv_sock_f_dsc, buffer, 255);
     if (no_rcvd_bts < 0)
     {
       cerr << "Cannot read from socket" << endl;
     }
     cout << "[ ID ] " << buffer << endl;
-    // memcmp does not depend on the received data being null terminated, on comapred to strcmp
+    // memcmp does not depend on the received data being null terminated, on
+    // comapred to strcmp
     if ( memcmp(buffer, "9751914896", strlen("9751914896")) == 0)
     {
       t = time(0);
@@ -128,9 +135,10 @@ int main(int argc, char* argv[])
       strftime (tme, 20, "%g%m%d%H%M%S", now);
       // place container name
       CMD.replace(18, 12, tme);
-      CMD.replace(178, 12, tme);
-      tsk_cntr();
-
+      CMD.replace(170, 12, tme);
+      cntr_ip = tsk_cntr();
+      cout << "[CNTR][IP] " << cntr_ip << endl;
+      send(nv_sock_f_dsc, cntr_ip.c_str(), 13, 0);
     }
     else
     {
@@ -140,10 +148,28 @@ int main(int argc, char* argv[])
   }
 
   cout << "\nClosing thread and conn" << endl;
-  close(nov_sock_fl_desc);
+  close(nv_sock_f_dsc);
 }
-
-void tsk_cntr ()
+// tsk_cntr function is used for management of individual calls from vehicle.
+///////////////////////////
+// Derive data-point for each container from here.
+// Return type may be changed to reflect complex functions.
+///////////////////////////
+string tsk_cntr ()
 {
-  system(CMD.c_str());
+  return exec(CMD.c_str());
+}
+// exec() for writing stdout of sys_call into program
+string exec(const char* cmd)
+{
+  array<char, 128> buffer;
+  string result;
+  shared_ptr<FILE> pipe(popen(cmd, "r"), pclose);
+  if (!pipe) throw runtime_error("popen() failed!");
+  while (!feof(pipe.get()))
+  {
+    if(fgets(buffer.data(), 128, pipe.get()) != nullptr)
+      result += buffer.data();
+  }
+  return result;
 }
